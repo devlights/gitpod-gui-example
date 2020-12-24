598abe53986bb6b9ac0dbeb4575b28da8bc409b1
devlights <gsf.zero1@devlights.onmicrosoft.com>
2020-12-24 02:41:15 +0000

    Add sample source file

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/devlights/gitpod-gui-example
 
 go 1.15
 
-require fyne.io/fyne v1.4.2 // indirect
+require fyne.io/fyne v1.4.2
---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"os"
+
+	"fyne.io/fyne"
+	"fyne.io/fyne/app"
+	"fyne.io/fyne/widget"
+)
+
+func main() {
+	os.Exit(run())
+}
+
+func run() int {
+	setupUI()
+	return 0
+}
+
+func setupUI() {
+	a := app.New()
+	defer a.Quit()
+
+	w := a.NewWindow("Hello Fyne")
+	w.Resize(fyne.NewSize(200, 100))
+	w.SetContent(widget.NewVBox(
+		widget.NewLabel("hello world"),
+		widget.NewButton("quit", func() {
+			w.Close()
+		}),
+	))
+
+	w.ShowAndRun()
+}
